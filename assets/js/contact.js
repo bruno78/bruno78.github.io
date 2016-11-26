0a147006d41643d98dd386bf95a4ed0c2f5d97da
bruno78 <devbruno78@gmail.com>
2016-11-26 12:48:05 -0500

    updated some posts

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,3 +1,16 @@
-// Avoiding exposing email
-var contactform =  document.getElementById('contactform');
-    contactform.setAttribute('action', '//formspree.io/' + 'me' + '@' + 'brunogtavares' + '.' + 'com');
+$(document).ready(function() {
+  $("#submit").click(function() {
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+    var contact = $("#contact").vacl();
+    $("#returnmessage").empty();
+    if (name == '' || email == '' || contact == '') {
+      alert("Please Fill Required Fields");
+    } else {
+      // Avoiding exposing email
+      var contactform =  document.getElementById('contactform');
+          contactform.setAttribute('action', '//formspree.io/' + 'me' + '@' + 'brunogtavares' + '.' + 'com');
+    }  
+  });
+});
