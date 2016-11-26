403cdbe0bf41958a8cf7d7cdb0786f30b3f09cbe
bruno78 <devbruno78@gmail.com>
2016-11-26 13:05:19 -0500

    fixed some issues

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,19 +1,3 @@
-$(document).ready(function() {
-  $("#submit").click(function() {
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var message = $("#message").val();
-    var contact = $("#contact").vacl();
-    $("#returnmessage").empty();
-    if (name == '' || email == '' || contact == '') {
-      alert("Please Fill Required Fields");
-    } else {
-      // Avoiding exposing email
-      var contactform =  document.getElementById('contactform');
-          contactform.setAttribute('action', '//formspree.io/' + 'me' + '@' + 'brunogtavares' + '.' + 'com');
-      $("form").append("<div class=\"hidden\">
-        <input type='hidden' name='_next' value=\"{{ \"/thankyou.html\" | prepend: site.baseurl }}\">
-      </div>");
-    }
-  });
-});
+// Avoiding exposing email
+var contactform =  document.getElementById('contactform');
+    contactform.setAttribute('action', '//formspree.io/' + 'me' + '@' + 'brunogtavares' + '.' + 'com');
